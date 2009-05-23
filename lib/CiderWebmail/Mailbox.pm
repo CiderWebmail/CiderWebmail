ce470f39e9f955b2c751a8940f7abd82228b4ddd
mathias <mathias@d1416938-2d58-0410-a1bb-95aaf470f215>
2009-05-23 10:53:38 +0000

    * add $mailbox->uids(); * remove whitespace
    git-svn-id: svn+ssh://organiceit.lanscene.at/srv/svn/CiderWebmail/trunk@164 d1416938-2d58-0410-a1bb-95aaf470f215

--- lib/CiderWebmail/Mailbox.pm
+++ lib/CiderWebmail/Mailbox.pm
@@ -27,12 +27,18 @@ sub mailbox {
 
 sub list_messages_hash {
     my ($self, $c, $o) = @_;
-  
+    
     if (defined($self->{uids})) {
         return $c->model->get_headers_hash($c, { mailbox => $self->{mailbox}, uids => $self->{uids}, headers => [qw/From Subject Date/] });
     } else {
         return $c->model->get_headers_hash($c, { mailbox => $self->{mailbox}, sort => $o->{sort}, headers => [qw/From Subject Date/] });
     }
+}
+
+sub uids {
+    my ($self, $c, $o) = @_;
+
+    return $c->model->get_folder_uids($c, { mailbox => $self->{mailbox} });
 }
 
 sub simple_search {
