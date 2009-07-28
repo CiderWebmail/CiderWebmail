4ca649f9c765a823e9d8af919c9b77951a9fa63c
nine <nine@d1416938-2d58-0410-a1bb-95aaf470f215>
2009-07-28 19:28:02 +0000

    * Restructured css and js code: moved message display stuff in it's own files. * Updated TODO with post 1.0 stuff
    git-svn-id: svn+ssh://organiceit.lanscene.at/srv/svn/CiderWebmail/trunk@208 d1416938-2d58-0410-a1bb-95aaf470f215

--- root/static/scripts/base.js
+++ root/static/scripts/base.js
@@ -1,3 +1,8 @@
+function open_in_new_window(link) {
+    window.open(link);
+    return false;
+}
+
 function add_attachment(adder) {
     var attachment = adder.parentNode;
     attachment.parentNode.insertBefore(attachment.cloneNode(true), attachment.nextSibling);
