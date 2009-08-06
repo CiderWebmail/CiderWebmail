547ff4fb7e54f8f062f5be71ab8e7f373c371287
Stefan Seifert <nine@detonation.org>
2009-08-06 15:24:01 +0200

    Ported auth store to Moose and documented
    Preparations for publishing on CPAN:
    * ported Catalyst::Authentication::Store::IMAP and Catalyst::Authentication::Store::IMAP::User to Moose
    * documented these two modules

--- lib/Catalyst/Authentication/Store/IMAP/User.pm
+++ lib/Catalyst/Authentication/Store/IMAP/User.pm
@@ -1,31 +1,54 @@
 package Catalyst::Authentication::Store::IMAP::User;
 
-use strict;
-use warnings;
+=head1 NAME
 
-use base qw/Catalyst::Authentication::User/;
+Catalyst::Authentication::Store::IMAP::User - An authentication user for IMAP.
+
+=head1 SYNOPSIS
+
+	use Catalyst::Authentication::Store::IMAP::User;
+	
+	Catalyst::Authentication::Store::IMAP::User->new(
+		id => "username",
+	);
+
+=head1 DESCRIPTION
+
+This implementation of authentication user handles is supposed to go hand in
+hand with L<Catalyst::Authentication::Store::IMAP>.
+
+=cut
+
+use Moose;
 use Mail::IMAPClient;
 
-sub new {
-    my $class = shift;
+extends qw/Catalyst::Authentication::User/;
 
-    bless { ( @_ > 1 ) ? @_ : %{ $_[0] } }, $class;
-}
+has id => (is => 'ro', isa => 'Str');
 
-# this class effectively handles any method calls
-sub can { 1 }
+=head1 METHODS
 
-sub id {
-    my $self = shift;
-    $self->{id};
-}
+=head2 new( @pairs )
+
+Create a new object with the key-value-pairs listed in the arg list.
+
+=head2 supports( )
+
+Checks for existence of keys that correspond with features.
+
+=cut
 
 sub supported_features {
     return {
-#        session => 1,
-        roles   => 1,
+        roles => 1,
     };
 }
+
+=head2 for_session( )
+
+Just returns $self, expecting it to be serializable.
+
+=cut
 
 sub for_session {
     my $self = shift;
@@ -33,16 +56,40 @@ sub for_session {
     return $self; # we serialize the whole user
 }
 
+=head2 from_session( )
+
+Just passes returns the unserialized object, hoping it's intact.
+
+=cut
+
 sub from_session {
     my ( $self, $c, $user ) = @_;
-    $user;
+
+    return $user;
 }
+
+=head2 store( )
+
+Accessors that override superclass's dying virtual methods.
+
+=cut
+
+=head2 id( )
+
+=cut
+
+=head2 check_password( $password )
+
+Establishes a connection to the IMAP server and checks the given user credentials.
+Stores the Mail::IMAPClient object on the stash as imapclient for usage by other components.
+
+=cut
 
 sub check_password {
     my ($self, $password) = @_;
 
     my $id = $self->id;
-    my $c = $self->{c};
+    my $c  = $self->{c};
 
     my %connect_info = (
         Server  => $c->config->{authentication}{realms}{imap}{store}{host},
@@ -78,62 +125,20 @@ sub check_password {
 
 1;
 
-__END__
-
-=pod
-
-=head1 NAME
-
-Catalyst::Authentication::User::Hash - An easy authentication user
-object based on hashes.
-
-=head1 SYNOPSIS
-
-	use Catalyst::Authentication::User::Hash;
-	
-	Catalyst::Authentication::User::Hash->new(
-		password => "s3cr3t",
-	);
-
-=head1 DESCRIPTION
-
-This implementation of authentication user handles is supposed to go hand in
-hand with L<Catalyst::Authentication::Store::Minimal>.
-
-=head1 METHODS
-
-=head2 new( @pairs )
-
-Create a new object with the key-value-pairs listed in the arg list.
-
-=head2 supports( )
-
-Checks for existence of keys that correspond with features.
-
-=head2 for_session( )
-
-Just returns $self, expecting it to be serializable.
-
-=head2 from_session( )
-
-Just passes returns the unserialized object, hoping it's intact.
-
-=head2 AUTOLOAD( )
-
-Accessor for the key whose name is the method.
-
-=head2 store( )
-
-Accessors that override superclass's dying virtual methods.
-
-=head2 id( )
-
-=head2 can( )
-
 =head1 SEE ALSO
 
 L<Hash::AsObject>
 
 =cut
 
+=head1 AUTHOR
+
+Stefan Seifert <nine@cpan.org>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
