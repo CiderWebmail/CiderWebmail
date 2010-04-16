a4628d1c5e77454103d55efebdfc350f44d83082
Mathias Reitinger <mathias.reitinger@loop0.org>
2010-04-16 14:46:29 +0200

    move html processing code to HTML::Cleaner

--- lib/CiderWebmail/Part/TextHtml.pm
+++ lib/CiderWebmail/Part/TextHtml.pm
@@ -1,8 +1,8 @@
 package CiderWebmail::Part::TextHtml;
 
 use Moose;
-use HTML::Tidy;
-use HTML::Scrubber;
+
+use HTML::Cleaner;
 
 extends 'CiderWebmail::Part';
 
@@ -17,26 +17,9 @@ sub render {
 
     die 'no part set' unless defined $self->body;
 
-    my $tidy = HTML::Tidy->new( { output_xhtml => 1, bare => 1, clean => 1, doctype => 'omit', enclose_block_text => 1, show_errors => 0, char_encoding => 'utf8', show_body_only => 1, tidy_mark => 0 } );
-    my $scrubber = HTML::Scrubber->new( allow => [ qw/p b strong i u hr br div span table thead tbody tr th td/ ] );
+    my $cleaner = HTML::Cleaner->new({ input => $self->body });
 
-    my @default = (
-        0 => # default rule, deny all tags
-        {
-            '*' => 0, # default rule, deny all attributes
-            'href' => qr{^(?! (?: java)? script )}ixm,
-            'src' => qr{^(?! (?: java)? script )}ixm,
-            'class' => 1,
-            'style' => 1,
-        }
-    );
-    
-    $scrubber->default( @default );
-
-    my $content = $scrubber->scrub($self->body);
-    $content = $tidy->clean($content);
-
-    return $self->c->view->render_template({ c => $self->c, template => 'TextHtml.xml', stash => { part_content => $content } });
+    return $self->c->view->render_template({ c => $self->c, template => 'TextHtml.xml', stash => { part_content => $cleaner->process } });
 }
 
 =head2 content_type()
---
+++ lib/HTML/Cleaner.pm
@@ -0,0 +1,35 @@
+package HTML::Cleaner;
+
+use Moose;
+
+use HTML::Tidy;
+use HTML::Scrubber;
+
+has input => ( is => 'ro', isa => 'Str' );
+
+sub process {
+    my ($self) = @_;
+
+    die unless $self->input;
+
+    my $tidy = HTML::Tidy->new( { output_xhtml => 1, bare => 1, clean => 1, doctype => 'omit', enclose_block_text => 1, show_errors => 0, char_encoding => 'utf8', show_body_only => 1, tidy_mark => 0 } );
+    my $scrubber = HTML::Scrubber->new( allow => [ qw/p b strong i u hr br div span table thead tbody tr th td/ ] );
+
+    my @default = (
+        0 => # default rule, deny all tags
+        {
+            '*' => 0, # default rule, deny all attributes
+            'href' => qr{^(?! (?: java)? script )}ixm,
+            'src' => qr{^(?! (?: java)? script )}ixm,
+            'class' => 1,
+            'style' => 1,
+        }
+    );
+    
+    $scrubber->default( @default );
+
+    my $content = $scrubber->scrub($self->input);
+    return $tidy->clean($content);
+}
+
+1;
