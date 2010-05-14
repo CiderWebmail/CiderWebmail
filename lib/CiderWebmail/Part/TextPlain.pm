11682aa8b45fa0edc025e44ea28c64c32a74c4fb
Mathias Reitinger <mathias.reitinger@loop0.org>
2010-05-14 11:05:15 +0200

    convert URIs in text/plain messages to <a> tags

--- lib/CiderWebmail/Part/TextPlain.pm
+++ lib/CiderWebmail/Part/TextPlain.pm
@@ -3,6 +3,9 @@ package CiderWebmail::Part::TextPlain;
 use Moose;
 use Petal;
 
+use Regexp::Common qw /URI/;
+use HTML::Entities;
+
 extends 'CiderWebmail::Part';
 
 =head2 render()
@@ -15,8 +18,13 @@ sub render {
     my ($self) = @_;
 
     die 'no part set' unless defined $self->body;
+
+    my $content = $self->body;
+    $content =~ s/$RE{URI}{-keep}/<a href="$1">$1<\/a>/g;
+
+    HTML::Entities::encode_entities($content);
     
-    return $self->c->view->render_template({ c => $self->c, template => 'TextPlain.xml', stash => { part_content => Text::Flowed::reformat($self->body) } });
+    return $self->c->view->render_template({ c => $self->c, template => 'TextPlain.xml', stash => { part_content => $content } });
 }
 
 =head2 content_type()
