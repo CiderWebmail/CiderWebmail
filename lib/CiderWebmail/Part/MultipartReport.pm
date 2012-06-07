3b61d60f4a356259e53c4315a2ab1037b4e002d4
Mathias Reitinger <mathias.reitinger@loop0.org>
2012-06-07 20:46:57 +0200

    refactor duplicated multipart handling code into MultipartGeneric.pm

--- lib/CiderWebmail/Part/MultipartReport.pm
+++ lib/CiderWebmail/Part/MultipartReport.pm
@@ -2,24 +2,8 @@ package CiderWebmail::Part::MultipartReport;
 
 use Moose;
 
-extends 'CiderWebmail::Part';
-has renderable          => (is => 'rw', isa => 'Bool', default => 0 );
-has render_by_default   => (is => 'rw', isa => 'Bool', default => 0 );
-has message             => (is => 'rw', isa => 'Bool', default => 0 );
+extends 'CiderWebmail::Part::MultipartGeneric';
 
 sub supported_type { return 'multipart/report'; }
-
-sub load_children {
-    my ($self) = @_;
-
-    return unless defined $self->bodystruct->{bodystructure};
-
-    foreach(@{ $self->bodystruct->{bodystructure} }) {
-        my $part = $self->handler({ bodystruct => $_ });
-
-        push(@{ $self->parent_message->{children} }, $part) if $part;
-        $self->root_message->parts->{$part->id} = $part;
-    }
-}
 
 1;
