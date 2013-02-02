214a93ebbfca8d08df3b8d2f0357d1ad52180773
Mathias Reitinger <mathias.reitinger@loop0.org>
2013-02-02 20:25:51 +0100

    remove accidental commit from mooTagify testing

--- root/static/scripts/mail/compose.js
+++ root/static/scripts/mail/compose.js
@@ -5,30 +5,3 @@ window.addEvent('load', function() {
         errorPrefix: "",
     });
 });
-
-window.addEvent("domready", function() {
-    var tagify = new mooTagify(document.id("tagWrap"), null, {
-        availableOptions: ['bar','barman','barmaid','bartender','FooBar','Gay Bar','crowbar','this','I like this'],
-        tagEls: 'span.btn.btn-mini',
-        closeEls: 'button.close.btn-small.remove-tag',
-        autoSuggest: true,
-        tags: 'foo,bar,this rocks',
-        /* tags: function() {
-            return this.listTags.get('value').clean();
-        },*/
-        // persist: false,
-        // addOnBlur: false, // only works via enter to add.
-        onInvalidTag: function(invalidTag) {
-            console.log(invalidTag + " was rejected due to length");
-        },
-        onLimitReached: function(rejectedTag) {
-            console.log(rejectedTag + " was not added, you have reached the maximum allowed tags count of " + this.options.maxItemCount);
-        }
-    });
-
-    document.id("getTags").addEvent("click", function() {
-        console.log(tagify.getTags(), tagify.getTags().join(","));
-    });
-
-});
-
