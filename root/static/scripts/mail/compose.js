b85409554a6ed761d403e7317aa50952c9210cf4
Mathias Reitinger <mathias.reitinger@loop0.org>
2013-04-04 18:35:08 +0200

    begin progress display for form submission

--- root/static/scripts/mail/compose.js
+++ root/static/scripts/mail/compose.js
@@ -5,3 +5,23 @@ window.addEvent('load', function() {
         errorPrefix: "",
     });
 });
+
+function send_mail(compose_form) {
+    //if we support FormData/xhr2
+    if (window.FormData !== undefined ) {
+        var mail_form = new FormData(compose_form);
+      
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', compose_form.action, true);
+      
+        xhr.upload.addEventListener("progress", function(e) {
+            var percent_completed = parseInt(e.loaded / e.total * 100);
+            document.getElementById('send_mail_progress').innerHTML = "Submission " + percent_completed + "% completed";
+        }, false);
+
+        console.log('sending to ' + compose_form.action);
+        xhr.send(mail_form);
+    } else { //othersize just fallback
+        compose_form.submit();
+    }
+}
